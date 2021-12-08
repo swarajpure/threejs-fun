8b5de1209cdcdd55c1f6ab541d29059ee6016c88
Swaraj <swarajrajpure@gmail.com>
2021-12-09 03:00:32 +0530

    chore: add texture and increase sphere speed

--- script.js
+++ script.js
@@ -2,8 +2,8 @@ import * as THREE from 'https://cdn.skypack.dev/three@0.135.0';
 
 const scene = new THREE.Scene();
 
-const ballGeometry = new THREE.SphereGeometry(5,50,50);
-const ballTexture = new THREE.TextureLoader().load('chess-texture.png');
+const ballGeometry = new THREE.SphereGeometry(5,32,32);
+const ballTexture = new THREE.TextureLoader().load('stone-texture.jpg');
 const ballMaterial = new THREE.MeshBasicMaterial({ map: ballTexture });
 const ball = new THREE.Mesh(ballGeometry, ballMaterial);
 
@@ -22,16 +22,16 @@ camera.position.setY(20)
 camera.lookAt(0,0,0)
 
 const groundGeometry = new THREE.PlaneGeometry( window.innerWidth, window.innerHeight );
-const groundTexture = new THREE.TextureLoader().load('grass-texture.jpg');
-const groundMaterial = new THREE.MeshBasicMaterial( {color: 0xaaaaa, map: groundTexture } );
+const groundTexture = new THREE.TextureLoader().load('stone-texture.jpg');
+const groundMaterial = new THREE.MeshBasicMaterial( {color: 0x666666  , map: groundTexture });
 const ground = new THREE.Mesh( groundGeometry, groundMaterial );
 ground.rotateX(- Math.PI / 2)
 ground.position.setY(-20)
-scene.add( ground );
+scene.add(ground);
 
 document.addEventListener('keydown', (e) => {
-  const defaultMovement = 1;
-  const defaultRotation = 0.5;
+  const defaultMovement = 5;
+  const defaultRotation = 0.25;
   switch(e.key) {
     case 'ArrowUp':
       ball.position.z += -1 * defaultMovement;
